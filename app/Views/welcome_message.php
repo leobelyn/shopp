43e5b713f72176e14870b576a15758781001931f
leobelyn <leobelyng@gmail.com>
2023-10-01 20:32:56 +0700

    main

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,12 @@
+<?= $this->include('include/top') ?>
+<div id="mobile-menu-wrap"></div>
+        <div class="offcanvas__text">
+            <p>Free shipping, 30-day return or refund guarantee.</p>
+        </div>
+        <?= $this->include('include/header') ?>
+        <?= $this->include('include/banner') ?>
+        <?= $this->include('include/products') ?>
+        <?= $this->include('include/categories') ?>
+        <?= $this->include('include/blog') ?>
+</div>
+<?= $this->include('include/end') ?>
